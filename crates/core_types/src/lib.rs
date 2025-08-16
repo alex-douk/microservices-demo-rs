8d3161275106a0cc965f02b860aaf1dd7d4d9f5f
Alexandre Doukhan <alex.doukh@gmail.com>
2025-08-16 02:56:55 +0200

    Backend shared types across services

---
+++ crates/core_types/src/lib.rs
@@ -0,0 +1,60 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct CartItem {
+    pub product_id: String,
+    pub quantity: i32,
+}
+
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Money {
+    // The 3-letter currency code defined in ISO 4217.
+    pub currency_code: String,
+    //The whole units of the amount.
+    // For example if `currencyCode` is `"USD"`, then 1 unit is one US dollar.
+    pub units: i64,
+    //Number of nano (10^-9) units of the amount.
+    // The value must be between -999,999,999 and +999,999,999 inclusive.
+    // If `units` is positive, `nanos` must be positive or zero.
+    // If `units` is zero, `nanos` can be positive, zero, or negative.
+    // If `units` is negative, `nanos` must be negative or zero.
+    // For example $-1.75 is represented as `units`=-1 and `nanos`=-750,000,000.
+    pub nanos: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct OrderItem {
+    pub item: CartItem,
+    pub cost: Money,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct Address {
+    pub street_address: String,
+    pub city: String,
+    pub state: String,
+    pub country: String,
+    pub zip_code: i32
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct CreditCardInfo {
+    pub credit_card_number: String,
+    pub credit_card_cvv: i32,
+    pub credit_card_expiration_year: i32,
+    pub credit_card_expiration_month: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct OrderResult {
+    pub order_id: String,
+    pub shipping_tracking_id: String,
+    pub shipping_cost: Money,
+    pub shipping_address: Address,
+    pub items: Vec<OrderItem>
+
+}
+
+pub mod money;
---
+++ crates/core_types/src/money.rs
@@ -0,0 +1,102 @@
+use std::fmt::Display;
+
+use crate::Money;
+
+const MIN_NANO: i32 = -999_999_999;
+const MAX_NANO: i32 = 999_999_999;
+const NANOS_MOD: i32 = 1_000_000_000;
+
+#[derive(Debug, Clone)]
+pub enum MoneyErrors {
+    InvalidValue,
+    MismatchingCurrency,
+}
+
+impl Display for MoneyErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MoneyErrors::InvalidValue => {
+                write!(f, "{}", "One of the specified money values is invalid")
+            }
+            MoneyErrors::MismatchingCurrency => write!(f, "{}", "Mismatching currency code"),
+        }
+    }
+}
+
+impl std::error::Error for MoneyErrors {}
+
+pub fn sign_matches(m: &Money) -> bool {
+    m.nanos == 0 || m.units == 0 || (m.nanos < 0) == (m.units < 0)
+}
+
+pub fn valid_nanos(nanos: i32) -> bool {
+    MIN_NANO <= nanos && nanos <= MAX_NANO
+}
+
+pub fn is_valid(m: &Money) -> bool {
+    valid_nanos(m.nanos) && sign_matches(m)
+}
+
+pub fn is_positive(m: Money) -> bool {
+    is_valid(&m) && m.units > 0 || (m.units == 0 && m.nanos > 0)
+}
+
+pub fn is_negative(m: Money) -> bool {
+    is_valid(&m) && m.units < 0 || (m.units == 0 && m.nanos < 0)
+}
+
+pub fn are_same_currency(l: &Money, r: &Money) -> bool {
+    l.currency_code == r.currency_code && l.currency_code != ""
+}
+
+pub fn are_equal(l: &Money, r: &Money) -> bool {
+    l.currency_code == r.currency_code && l.units == r.units && l.nanos == r.nanos
+}
+
+pub fn negate(m: Money) -> Money {
+    Money {
+        currency_code: m.currency_code,
+        units: -m.units,
+        nanos: -m.nanos,
+    }
+}
+
+pub fn sum(l: &Money, r: &Money) -> Result<Money, MoneyErrors> {
+    if let false = (is_valid(&l) || is_valid(&r)) {
+        return Err(MoneyErrors::InvalidValue);
+    }
+    if let false = are_same_currency(&l, &r) {
+        return Err(MoneyErrors::MismatchingCurrency);
+    }
+
+    let mut units = l.units + r.units;
+    let mut nanos = l.nanos + r.nanos;
+
+    if (units == 0 && nanos == 0) || (units > 0 && nanos >= 0) || (units < 0 && nanos <= 0) {
+        //same sign <units, nanos>
+        units += (nanos / NANOS_MOD) as i64;
+        nanos = nanos % NANOS_MOD;
+    } else {
+        // different sign. nanos guaranteed to not to go over the limit
+        if units > 0 {
+            units -= 1;
+            nanos += NANOS_MOD;
+        } else {
+            units += 1;
+            nanos -= NANOS_MOD;
+        }
+    }
+    Ok(Money {
+        units,
+        nanos,
+        currency_code: l.currency_code.clone(),
+    })
+}
+
+pub fn slow_multiply(m: &Money, n: i32) -> Money {
+    let mut out = m.clone();
+    for _ in 0..n {
+        out = sum(&out, &m).expect("Couldn't sum the value to its aggregator");
+    }
+    out
+}
